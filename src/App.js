86afaa81ba64b3f3dee76fa63b85e5157229cfde
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-12 17:22:36 +0300

    Setting up CategorySelector component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Question from './components/Question';
 import CategorySelector from './components/CategorySelector';
 import ResultModal from './components/ResultModal';
@@ -8,18 +8,20 @@ import './App.css';
 export default function App() {
 
   const [question, setQuestion] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState('any');
 
-  const getQuestion = () => {
-    const url = 'https://opentdb.com/api.php?amount=1';
+  const getQuestion = useCallback(() => {
+    let url = 'https://opentdb.com/api.php?amount=1';
+    if (selectedCategory !== 'any') url += `&category=${selectedCategory}`;
 
     fetch(url)
       .then(res => res.json())
       .then(data => setQuestion(data.results[0]))
-  }
+  }, [selectedCategory])
 
   useEffect(() => {
     getQuestion();
-  }, []);
+  }, [getQuestion, selectedCategory]);
 
   return (
     <div className="app">
@@ -28,7 +30,10 @@ export default function App() {
 
       {/* question header ----------------------- */}
       <div className="question-header">
-        <CategorySelector />
+        <CategorySelector
+          category={selectedCategory}
+          chooseCategory={setSelectedCategory}
+        />
         <Scoreboard />
       </div>
 
