6f129b298cae6a62b7a96035bbfbe7671149c445
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-12 18:37:47 +0300

    Updates

---
+++ precache-manifest.a7f7c5ca33063ea6e0a24dcf5ce7d6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415177ae6d85133dbd91f8a2b59db9ac",
+    "url": "/Trivia_Game/index.html"
+  },
+  {
+    "revision": "661b512ef91d9c206ec5",
+    "url": "/Trivia_Game/static/css/main.f474f262.chunk.css"
+  },
+  {
+    "revision": "39a083b72efdf16fbd45",
+    "url": "/Trivia_Game/static/js/2.a0893d78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Trivia_Game/static/js/2.a0893d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661b512ef91d9c206ec5",
+    "url": "/Trivia_Game/static/js/main.1f238b7e.chunk.js"
+  },
+  {
+    "revision": "31360bfed24c7ae7ca25",
+    "url": "/Trivia_Game/static/js/runtime-main.004a1fef.js"
+  }
+]);
